bb1dd98835d48bd3f76c060dec6768129e5c29d6
Daniel Pepper <pepper.daniel@gmail.com>
2023-03-20 21:29:55 -0700

    simplify

--- lib/rspec/protobuf/refinements.rb
+++ lib/rspec/protobuf/refinements.rb
@@ -5,7 +5,7 @@ module RSpec
         def include?(*args)
           expected_attrs = Hash === args.last ? args.pop : {}
 
-          # ensure all enumerated keys are present
+          # ensure all enumerated keys are indeed attributes
           return false unless args.all? { |attr| respond_to?(attr) }
 
           # ensure all enumerated attributes are present
@@ -14,64 +14,26 @@ module RSpec
           fields = {}
           self.class.descriptor.each { |f| fields[f.name.to_sym] = f }
 
-          # check expected attribute matches
+          # ensure that expected attribute matches
           expected_attrs.all? do |expected_attr, expected_value|
-            field = fields[expected_attr]
-            actual_value = field.get(self)
-
-            # convert symbols to strings
-            if field.type == :string && expected_value.is_a?(Symbol)
-              expected_value = expected_value.to_s
-            end
-
-            # convert enum to int value to match input type
-            if field.type == :enum && expected_value.is_a?(Integer)
-              actual_value = field.subtype.lookup_name(actual_value)
-            end
-
-            matches = expected_value === actual_value
-
-            if actual_value.is_a?(Google::Protobuf::MessageExts)
-              if expected_value.is_a?(Hash)
-                matches ||= actual_value.match?(**expected_value)
-              else
-                matches ||= expected_value === actual_value.to_h
-              end
-            end
-
-            matches
+            field_matches?(fields[expected_attr], expected_value)
           end
         end
 
         def match?(**attrs)
-          # ensure all enumerated keys are present
+          # ensure all enumerated attributes are present
           return false unless attrs.keys.all? { |attr| respond_to?(attr) }
 
-          # ensure each field matches
+          # ensure that each field matches
           self.class.descriptor.all? do |field|
-            actual_value = field.get(self)
-            expected_value = attrs[field.name.to_sym]
-
-            if actual_value.is_a?(Google::Protobuf::MessageExts) && expected_value.is_a?(Hash)
-              actual_value.match?(**expected_value)
+            expected = if attrs.key?(field.name.to_sym)
+              attrs[field.name.to_sym]
             else
-              unless attrs.key?(field.name.to_sym)
-                # fall back to default value
-                expected_value = field.label == :repeated ? [] : field.default
-              end
-
-              # convert symbols to strings
-              if field.type == :string && expected_value.is_a?(Symbol)
-                expected_value = expected_value.to_s
-              end
-
-              # convert enum to int value to match input type
-              if field.type == :enum && expected_value.is_a?(Integer)
-                actual_value = field.subtype.lookup_name(actual_value)
-              end
-
-              expected_value === actual_value
+              # use default value
+              field.label == :repeated ? [] : field.default
             end
+
+            field_matches?(field, expected)
           end
         end
 
@@ -91,6 +53,37 @@ module RSpec
           end
 
           res
+        end
+
+        private
+
+        def field_matches?(field, expected)
+          actual = field.get(self)
+
+          # convert symbols to strings
+          if field.type == :string && expected.is_a?(Symbol)
+            expected = expected.to_s
+          end
+
+          # convert enum to int value to match input type
+          if field.type == :enum && expected.is_a?(Integer)
+            actual = field.subtype.lookup_name(actual)
+          end
+
+          if actual.is_a?(Google::Protobuf::MessageExts)
+            case expected
+            when Google::Protobuf::MessageExts
+              expected === actual
+            when Hash
+              actual.match?(**expected)
+            else
+              # eg. RSpec matchers
+              expected === actual.to_h
+            end
+          else
+            # compare scalar attribute
+            expected === actual
+          end
         end
       end
     end
